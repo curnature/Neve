2f86ee9cef26b7fb2863a1c48d4c84f1d8383162
curnature <curnature00@gmail.com>
2025-09-06 16:24:20 -0400

    jdtls modify

--- config/languages/nvim-jdtls.nix
+++ config/languages/nvim-jdtls.nix
@@ -8,19 +8,20 @@ in
     nvim-jdtls.enable = lib.mkEnableOption "Enable nvim-jdtls module";
   };
   config = lib.mkIf config.nvim-jdtls.enable {
-    plugins.nvim-jdtls = {
+    plugins.jdtls = {
       enable = true;
-      cmd = [
-        "/nix/store/ka32hdd6lnbs1ahndn64p5519381r2vl-jdt-language-server-1.44.0/bin/jdtls"
-      ];
-      # configuration = "/path/to/configuration";
-      data = "~/.cache/jdtls/workspace";
       settings = {
+        cmd = [
+          "/nix/store/ka32hdd6lnbs1ahndn64p5519381r2vl-jdt-language-server-1.44.0/bin/jdtls"
+          "-data" "~/.cache/jdtls/workspace"
+        ];
         java = {
           signatureHelp = true;
           completion = true;
         };
+
       };
+      # configuration = "/path/to/configuration";
       initOptions = {
         bundles = [
           "/nix/store/b9ib40q36wxjl4xs5lng263lflz1fsi7-vscode-extension-vscjava-vscode-java-debug-0.49.2023032407/share/vscode/extensions/vscjava.vscode-java-debug/server/com.microsoft.java.debug.plugin-0.44.0.jar"
